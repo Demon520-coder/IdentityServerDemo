f897dd9f82c4889c00cc02fb347912fddf50a11d
99105337@qq.com <99105337@qq.com>
2020-05-14 22:48:36 +0800

    添加项目文件。

---
+++ Idsrv4/InMemoeryConfig.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Idsrv4
+{
+    public class InMemoeryConfig
+    {
+        /// <summary>
+        /// 定义受保护的Api资源
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("ApiRes1","FirstApiResource"),
+                new ApiResource("ApiRes2","SecondApiResource")
+            };
+        }
+
+        /// <summary>
+        /// 定义被授权的客户端
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] {
+               new Client
+               {
+                   ClientId="api1",
+                   ClientName="API1",
+                   ClientSecrets={new Secret("api1".Sha256())},
+                   AllowedGrantTypes=GrantTypes.ClientCredentials,
+                   AllowedScopes={ "ApiRes1" }
+               },
+               new Client
+               {
+                   ClientId="api2",
+                   ClientName="API2",
+                   ClientSecrets={new Secret("api2".Sha256())},
+                   AllowedGrantTypes=GrantTypes.ClientCredentials,
+                   AllowedScopes={"Api2Res2"}
+               }
+            };
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,70 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await ApiTest("http://localhost:5001/api/values");
+        }
+
+
+        static async Task ApiTest(string apiUrl)
+        {
+            
+            using (var discoveryClient = new HttpClient())
+            {
+                DiscoveryDocumentRequest req = new DiscoveryDocumentRequest();
+                req.Address = "http://localhost:5000";
+                req.Method = HttpMethod.Get;
+                //发现认证服务
+                var discovery = await discoveryClient.GetDiscoveryDocumentAsync(req);
+                var nl = Environment.NewLine;
+                if (discovery.IsError)
+                {
+                    Console.WriteLine("======discoveryError=======" + nl);
+                    Console.WriteLine($"======{discovery.Error}======" + nl);
+                    Console.WriteLine("======discoveryError=======" + nl);
+                    return;
+                }
+
+                ClientCredentialsTokenRequest tokenRequest = new ClientCredentialsTokenRequest
+                {
+                    Address = discovery.TokenEndpoint,
+                    ClientId = "api1",
+                    ClientSecret = "api1",
+                    Scope = "ApiRes1",
+                    GrantType = IdentityModel.OidcConstants.GrantTypes.ClientCredentials
+                };
+
+                //获取token
+                var tokenResponse = await discoveryClient.RequestClientCredentialsTokenAsync(tokenRequest);
+
+                if (tokenResponse.IsError)
+                {
+                    Console.WriteLine("======tokenResponseError=======" + nl);
+                    Console.WriteLine($"======{tokenResponse.Error}======" + nl);
+                    Console.WriteLine("======tokenResponseError=======" + nl);
+                    return;
+                }
+
+                using (var apiClient = new HttpClient())
+                {
+                    //设置token
+                    apiClient.SetBearerToken(tokenResponse.AccessToken);
+                    //请求api
+                    var result = await apiClient.GetStringAsync(apiUrl);
+
+                    Console.WriteLine("======apiResult=======" + nl);
+                    Console.WriteLine($"======{result}======" + nl);
+                    Console.WriteLine("======apiResult=======" + nl);
+                }
+            }
+        }
+    }
+}
